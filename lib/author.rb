9cca0a2e9a188508de95f74e6ef0f5f686afffe0
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-25 23:25:03 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,19 +1,24 @@
 class Author
-  attr_accessor :name 
-  @@all = []
+  attr_accessor :name, :posts
+  #@@all = []
+  @@post_count = 0
   
   def initialize(name)
     @name = name 
-    @@all << self 
+    @posts = []
+    #@@all << self 
   end
   
-  def self.all 
-    @@all
-  end
+  #def self.all 
+   # @@all
+  #end
+  
+
    
   def add_post(post)
     post.artist = self
     @@post_total += 1
+    #@posts << post
   end  
   
   def posts 
@@ -21,15 +26,16 @@ class Author
       post.artist == self
     end
   end
-  
 
-  def add_post_by_title(post_name)
-    post = Post.new(post_name)
-    post.artist = self
-    @@post_total += 1
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
   end
 
   def self.post_count
-    @@post_total
+    @@post_count
   end
+
 end
