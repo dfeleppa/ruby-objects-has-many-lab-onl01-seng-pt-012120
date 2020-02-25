06eeafc1b02020a979e27574b33ae83ea888a736
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-25 23:28:08 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,14 @@
-
 class Author 
-  attr_accessor :name, posts
+  attr_accessor :name
   @@post_count = 0
 
   def initialize(name)
     @name = name
     @posts = []
+  end
+
+  def posts
+    @posts
   end
 
   def add_post(post)
@@ -24,5 +27,4 @@ class Author
   def self.post_count
     @@post_count
   end
-
 end
