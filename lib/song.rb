a6623d8f2911707ff95364573a3d7e5f54d13b33
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-25 23:25:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,3 +12,19 @@ class Song
   end
   
 end
+class Song
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def artist_name
+    if artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+end
