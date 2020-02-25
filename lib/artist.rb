47a5157b4d53145f263a258359bd17f03809099a
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-25 23:34:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,38 +1,32 @@
-class Artist 
-  attr_accessor :name, :songs 
-  @@all = []
-  @@song_total
-  
+
+class Artist
+  attr_accessor :name
+
+  @@song_count = 0
+
   def initialize(name)
     @name = name
-    @@all << self
-    @@song_total = 0
-  end
-
-  def self.all  
-    @@all
-  end
-  
-  def add_song(song)
-    song.artist = self
-    @@song_total += 1
-  end  
-  
-  def songs 
-    Song.all.select do |song|
-      song.artist == self
-    end
-  end
-  
-
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    song.artist = self
-    @@song_total += 1
+    @songs = []
   end
 
   def self.song_count
-    @@song_total
+    @@song_count
+  end
+  
+  def songs
+    @songs
   end
 
+  def add_song(song_name)
+    @songs << song_name
+    song_name.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 end
